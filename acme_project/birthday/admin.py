40980e68664b07dd9ab40f5421f42b1a4b2caaa6
Irina Poloubessov <polubes@gmail.com>
2023-09-21 13:42:17 +0200

    pagination with birthday list and images

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,9 @@
 from django.contrib import admin
+
+from .models import Birthday
+
+class BirthdayAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birthday',)
+    list_editable = ('birthday',)
+
+admin.site.register(Birthday, BirthdayAdmin)
